68dc1fc3dd203b922fa97a0d9e20e90b5bd4c5c1
Olixo <fuzzehbearr@live.com>
2021-10-23 03:16:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mytimelogapp/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.mytimelogapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText NameEditText;  //link to xml EditText in onCreate()
+    private EditText TimeEditText;
+    private EditText DateEditText;
+    private EditText TimeOutEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NameEditText = findViewById(R.id.NameEditText); // Linked all four to xmL's EditText to get info
+        TimeEditText = findViewById(R.id.TimeEditText);
+        DateEditText = findViewById(R.id.DateEditText);
+        TimeOutEditText = findViewById(R.id.TimeOutEditText);
+    }
+
+    public void onLogTimeButtonClick(View view) {
+        //Capture the info from NameEditText, TimeEditText, YearEditText and TimeOutEditText
+        String personName = NameEditText.getText().toString();
+        String personTime = TimeEditText.getText().toString();
+        String personDate = DateEditText.getText().toString();
+        String personTimeOut = TimeOutEditText.getText().toString();
+
+        //Create Person object from the information above
+        Person person;
+        String message;
+        if (!personName.trim().equals("")){ //if name is not empty, object will be created
+            //convert str to number
+            int time;
+            try {
+                time = Integer.parseInt(personTime);
+            } catch (NumberFormatException nfe) {
+                time = 830; // default to 830 if cannot be converted
+            }
+
+            int date;
+            try {
+                date = Integer.parseInt(personDate);
+            } catch (NumberFormatException nfe) {
+                date = 010121; //default to 010121 if cannot be converted
+            }
+
+            int timeout;
+            try {
+                timeout = Integer.parseInt(personTimeOut);
+            } catch (NumberFormatException nfe) {
+                timeout = 1830; //default to 1830 if cannot be converted
+            }
+
+            person = new Person(personName, time, date, timeout);
+            message = person.getMessage();
+        } else {
+            message = "Please enter a valid Name, Time, Date and Timeout";
+        }
+
+        //Displays the persons info using a Toast
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+    }
+}
---
+++ app/src/main/java/com/example/mytimelogapp/Person.java
@@ -0,0 +1,60 @@
+package com.example.mytimelogapp;
+
+public class Person {
+    // attributes
+    private String name;
+    private int time;
+    private int date;
+    private int timeOut;
+    private String message;
+
+    public Person() { //default constructor
+        this.name = "NoName";
+        this.time = 2358;
+        this.date = 01012021;
+        this. timeOut = 2359;
+        this.message = "Dafault parameters initiated: Name: " + name + ", Time: " + time + ", Date: " + date + ", TimeOut: " + timeOut;
+    }
+
+    public Person(String name) {
+        this.name = name;
+        this.time = 830;
+        this.date = 01012021;
+        this.timeOut = 1630; // give default time, date and timeOut
+        this.message = "Name has been given: " + name + "Time, Date and Timeout will use default values. Time : " + time + ", Date: " + date + ", Timeout: " + timeOut;
+    }
+
+
+    public Person(String name, int time, int date, int timeOut) {
+        this.name = name;
+        this.time = time;
+        this.date = date;
+        this.timeOut = timeOut;
+        this.message = "LOGGED. Your name: " + name + ", has checked in at: " + time + ", on the day: " + date + "and checked out at: " + timeOut;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public int getTimeOut() {
+        return timeOut;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
