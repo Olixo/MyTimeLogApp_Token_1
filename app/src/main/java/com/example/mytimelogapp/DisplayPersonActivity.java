2952865189b72325b99613f97fc7fb6f88aa670c
Olixo <fuzzehbearr@live.com>
2021-10-23 14:29:26 +0800

    Navigate through array with button that cycles through array index to display all the people that have logged time.

--- app/src/main/java/com/example/mytimelogapp/DisplayPersonActivity.java
+++ app/src/main/java/com/example/mytimelogapp/DisplayPersonActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 public class DisplayPersonActivity extends AppCompatActivity {
     private TextView displayPersonTextView;
     private ArrayList<Person>personArrayList;
+    private int currentPersonIndex; //To navigate the array Index
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +21,28 @@ public class DisplayPersonActivity extends AppCompatActivity {
 
         displayPersonTextView = findViewById(R.id.displayPersonTextView);
         Intent intent = getIntent();
-        ArrayList<Person> personArrayList = (ArrayList<Person>) intent.getSerializableExtra("allPersons");
+        personArrayList = (ArrayList<Person>) intent.getSerializableExtra("allPersons");
 
         String message = "";
+        currentPersonIndex = -1;
         if (personArrayList.size()!=0) {
             message = personArrayList.get(0).getMessage(); //getting first Person
+            currentPersonIndex = 0; //helps indicate if there are cars in the arraylist
         }
 
+
         displayPersonTextView.setText(message +  "\nNumber of logged times: " + personArrayList.size()); // how many people have logged time
+    }
+
+    public void onNextPersonButtonClick(View view) {
+
+        if (currentPersonIndex != -1) {
+            currentPersonIndex++; //go to next car
+            currentPersonIndex = currentPersonIndex%personArrayList.size();
+
+            Person person = personArrayList.get(currentPersonIndex);
+
+            displayPersonTextView.setText(person.getMessage());
+        }
     }
 }
