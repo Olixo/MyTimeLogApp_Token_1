824c55645caf7dbebaaeac9b3311b2d90b303d68
Olixo <fuzzehbearr@live.com>
2021-10-23 16:41:30 +0800

    Added "load" button to load the saved information that the "Save Info" button saved.

--- app/src/main/java/com/example/mytimelogapp/DisplayPersonActivity.java
+++ app/src/main/java/com/example/mytimelogapp/DisplayPersonActivity.java
@@ -4,16 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 public class DisplayPersonActivity extends AppCompatActivity {
     private TextView displayPersonTextView;
     private ArrayList<Person>personArrayList;
     private int currentPersonIndex; //To navigate the array Index
+
+
+    private  static final String TAG = "DisplayPersonActivity";
+
+    //File Details
+    private static final String FILE_NAME= "all_Logs.txt"; // <-- Name of file
+    private File file; // File object to perform task
+    private FileOutputStream outputStream;
+    private FileInputStream inputStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,8 @@ public class DisplayPersonActivity extends AppCompatActivity {
 
 
         displayPersonTextView.setText(message +  "\nNumber of logged times: " + personArrayList.size()); // how many people have logged time
+
+        file = new File(this.getFilesDir(), FILE_NAME); // creating a file object
     }
 
     public void onNextPersonButtonClick(View view) {
@@ -49,10 +64,50 @@ public class DisplayPersonActivity extends AppCompatActivity {
 
     public void onSaveButtonClick(View view) {
         if (personArrayList.size()>0) {
+            try {
+                outputStream = new FileOutputStream(file);
+                //creating a stream to connect to file object
+                int numPeople = personArrayList.size();
+                String data = "Number of people: " + numPeople;
+                //prepare the information to write
+
+                outputStream.write(data.getBytes());
+                //convert to bytes and write to outputstream
+                Log.d(TAG, "SAVING SUCCESS!!");
+                outputStream.close();
+
+            } catch (Exception e) {
+                Log.d(TAG, "WRITING FILE UNSUCCSESSFUL!!");
+                e.printStackTrace();
+            }
             Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show();
 
         } else {
             Toast.makeText(this, "No data to save", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void onLoadLogButtonClick(View view) {
+        // create byte array to hold information based on length
+        int length = (int) file.length();
+        byte[] bytes = new byte[length];
+
+        try {
+            //create fileinputstream object to read from file
+            inputStream = new FileInputStream(file);
+
+            //read into array
+            inputStream.read(bytes);
+            inputStream.close();
+
+            String data = new String(bytes); //converting bytes into string
+
+            Toast.makeText(getBaseContext(), data, Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            Log.d(TAG, "L");
+            e.printStackTrace();
+        }
+
     }
 }
